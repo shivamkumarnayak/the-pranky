0f2d13572d70c611cd0dfaebede387c4aaf743ce
shivamkumarnayak <shivamkumarnayak100@gmail.com>
2022-05-06 23:23:39 +0530

    Add features in extension

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,18 @@
+// Share content to other social media start
+let tweetMe = document.getElementById('tweet');
+
+let content = document.getElementById('jokeElement');
+
+const tweetNow = () =>{
+  console.log("Welcome To the twitter");
+  const tweetPost = `https://twitter.com/intent/tweet?text=${content.innerText}`;
+  window.open(tweetPost);
+}
+
+tweetMe.addEventListener('click', tweetNow);
+
+// Share content to other social media end
+
 // First time joke fetch 
 fetch('https://icanhazdadjoke.com/slack')
 .then(data => data.json())
